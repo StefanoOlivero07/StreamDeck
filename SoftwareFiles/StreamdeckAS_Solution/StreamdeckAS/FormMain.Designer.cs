b3d1dd675c749362c4d7c0c77e65856f967d24ed
StefanoOlivero07 <s.olivero.3215@vallauri.edu>
2024-12-28 22:32:13 +0100

    Aggiunte bozze dei file di Arduino e del programma

---
+++ SoftwareFiles/StreamdeckAS_Solution/StreamdeckAS/FormMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace StreamdeckAS
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnInizia = new System.Windows.Forms.Button();
+            this.btnTermina = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "StreamdeckAS";
+            // 
+            // btnInizia
+            // 
+            this.btnInizia.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInizia.Location = new System.Drawing.Point(75, 185);
+            this.btnInizia.Name = "btnInizia";
+            this.btnInizia.Size = new System.Drawing.Size(240, 85);
+            this.btnInizia.TabIndex = 1;
+            this.btnInizia.Text = "Inizia";
+            this.btnInizia.UseVisualStyleBackColor = true;
+            this.btnInizia.Click += new System.EventHandler(this.btnInizia_Click);
+            // 
+            // btnTermina
+            // 
+            this.btnTermina.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTermina.Location = new System.Drawing.Point(363, 185);
+            this.btnTermina.Name = "btnTermina";
+            this.btnTermina.Size = new System.Drawing.Size(240, 85);
+            this.btnTermina.TabIndex = 2;
+            this.btnTermina.Text = "Termina";
+            this.btnTermina.UseVisualStyleBackColor = true;
+            this.btnTermina.Click += new System.EventHandler(this.btnTermina_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 588);
+            this.Controls.Add(this.btnTermina);
+            this.Controls.Add(this.btnInizia);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "StreamdeckAS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnInizia;
+        private System.Windows.Forms.Button btnTermina;
+    }
+}
+
---
+++ SoftwareFiles/StreamdeckAS_Solution/StreamdeckAS/FormMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace StreamdeckAS
+{
+    public partial class FormMain : Form
+    {
+        SerialPort port;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInizia_Click(object sender, EventArgs e)
+        {
+            port = new SerialPort("COM3", 9600);
+            port.DataReceived += Port_DataReceived;
+            port.Open();
+
+            MessageBox.Show("Connessione stabilita");
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string input = port.ReadLine();
+
+            if (input.Trim() == "do")
+                Process.Start("chrome.exe", "https://randomuserolivero.altervista.org");
+        }
+
+        private void btnTermina_Click(object sender, EventArgs e)
+        {
+            port.Close();
+        }
+    }
+}
